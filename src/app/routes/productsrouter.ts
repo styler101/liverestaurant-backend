57f1c52c5ad4708ca741842ca31fc9fdb37ebeee
Lucas <lucas.santos@mundowap.com.br>
2022-11-19 10:32:55 -0300

    feat: ensure only admin can create a product

--- src/app/routes/productsrouter.ts
+++ src/app/routes/productsrouter.ts
@@ -1,7 +1,8 @@
 import { Router } from 'express'
 import upload from '../config/upload'
 import ProductController from '../controllers/Product/ProductController'
+import authmiddleware from '../middlewares/authmiddlewares'
 export const productRouter = Router()
 
 productRouter.get('/products', ProductController.index)
-productRouter.post('/product', upload.single('image'), ProductController.store)
+productRouter.post('/product', authmiddleware, upload.single('image'), ProductController.store)
