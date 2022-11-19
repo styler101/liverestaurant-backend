383273614eed6273a7c38a28f9fe533a6f38917c
Lucas <lucas.santos@mundowap.com.br>
2022-11-19 10:31:41 -0300

    feat: ensure only admin can create a category

--- src/app/routes/categoriesrouter.ts
+++ src/app/routes/categoriesrouter.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express'
 import CategoryController from '../controllers/Category/CategoryController'
 import CategoryProductController from '../controllers/Category/CategoryProductController'
+import authmiddleware from '../middlewares/authmiddlewares'
 
 const categoryRouter = Router()
 
@@ -9,7 +10,7 @@ const categoryRouter = Router()
 categoryRouter.get('/categories', CategoryController.index)
 // admin
 // Create category
-categoryRouter.post('/category', CategoryController.store)
+categoryRouter.post('/category', authmiddleware, CategoryController.store)
 
 categoryRouter.get('/categories/:categoryId/products', CategoryProductController.getProductByCategoryId)
 
